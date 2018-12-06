017068a17bdf15bc66bd9e42f2f17a2b4b3a646b
Gyrwoe <gyrwoe12@gmail.com>
2018-12-06 21:32:12 +0100

    J'ai dis :
    J'ADORE le poisson

--- models/users.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  var Users = sequelize.define('users', {
-    username: DataTypes.STRING
-  });
-  console.log("TEST");
-  return Users;
-};
