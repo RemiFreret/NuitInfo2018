2ea9db42360a3913d89b4dce73a6b353ecd6bdd3
Gyrwoe <gyrwoe12@gmail.com>
2018-12-06 22:57:17 +0100

    Alors
    On a un probleme

--- models/base.js
+++ models/base.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   var Base = sequelize.define('base', {
     stock: DataTypes.STRING,
-    etat: DataTypes.STRING
+    etat: DataTypes.NUMBER
   });
   console.log("TEST");
   return Base;
--- models/tachesRegulieres.js
+++ models/tachesRegulieres.js
@@ -3,8 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   var TachesRegulieres = sequelize.define('TachesRegulieres', {
     objectif: DataTypes.STRING,
     data: DataTypes.DATE,
-    rythme: DataTypes.NUMBER,
-    priorite: DataTypes.NUMBER
+    rythme: DataTypes.INTEGER,
+    priorite: DataTypes.INTEGER
   });
   console.log("TEST");
   return TachesRegulieres;
