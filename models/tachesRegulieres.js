972dca38efe6c73eab829f35d324c6dbe7db6edd
Gyrwoe <gyrwoe12@gmail.com>
2018-12-06 21:50:11 +0100

    Et en plus
    c'est bon pour la santé

--- models/tachesRegulieres.js
+++ models/tachesRegulieres.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var TachesRegulieres = sequelize.define('TachesRegulieres', {
-    stock: DataTypes.STRING,
+    objectif: DataTypes.STRING,
+    data: DataTypes.DATE,
+    rythme: DataTypes.NUMBER,
+    priorite: DataTypes.NUMBER
   });
   console.log("TEST");
   return TachesRegulieres;
