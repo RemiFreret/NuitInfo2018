5152430a234ed55cad7f166fba32a2bc1bd7daa8
Bliisters <quentin.flegeau@orange.fr>
2018-12-06 22:20:21 +0100

    template

---
+++ cockpit/route.js
@@ -0,0 +1,16 @@
+var models  = require('../models');
+var express = require('express');
+var router = express.Router();
+
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+// define the home page route
+router.get('/', function (req, res) {
+            res.render('cockpit-index');
+})
+
+module.exports = router;
