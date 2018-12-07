9bf5b72babfc11122618b7e3d489bbeb8cfeae90
Bliisters <quentin.flegeau@orange.fr>
2018-12-07 03:24:01 +0100

    LE NIGHT MODE ET LES LIENS DU TEMPLATEUH

--- public/assets/js/nightmode.js
+++ public/assets/js/nightmode.js
@@ -10,16 +10,16 @@ $( '#nightmode' ).click(function() {
       $('.sidebar').css('background-color', '#2C2F33');
       $('p').css('color', '#eee');
       $('.header-title').css('color', '#eee');
-      $('.logo-normal').css('color', '#eee!important');
+      $('.simple-text').css('color', '#2C2F33!important');
 } else {
       $('body').css('background-color', '#eee');
       $('.card').css('background-color', '#FFFFFF');
       $('td').css('color', '#3C4858');
-      $('.card-title').css('color', '#FFFFFF');
+      $('.card-title').css('color', '#3C4858');
       $('.dropdown-menu').css('background-color', '#FFFFFF');
       $('.sidebar').css('background-color', '#FFFFFF');
       $('p').css('color', '#2C2F33');
       $('.header-title').css('color', '#212529');
-      $('.logo-normal').css('color', '#2C2F33!important');
+      $('.simple-text').css('color', '#FFFFFF!important');
 }
 });
